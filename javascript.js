b66ed4346a3d6a301cf327416c041cac4a395751
Yvens Almeida <yvensalmeida21@gmail.com>
2024-05-29 16:40:22 -0300

    Primeiro Exercício em JS

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+const nome = prompt("Digite seu primeiro nome: ");
+const sobreNome = prompt("Digite seu sobrenome: ");
+const campoDeEstudo = prompt("Digite seu campo de estudo: ");
+const anoNascimento = prompt("Digite seu ano de nascimento: ");
+const idade = 2024 - anoNascimento;
+
+alert(`Seu nome é ${nome} ${sobreNome}, seu campo de estudo é ${campoDeEstudo} e sua idade é de ${idade} anos`);
+
+
